c2f4cb44061072611ed44c86221650d69916363b
him-hey <him.hey@passerellesnumeriques.org>
2022-11-12 12:25:21 +0700

    add user login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,10 +6,46 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     //
+    public function login(Request $request){
+        try {
+            $validateUser = Validator::make($request->all(),[
+                'email' => 'required|email',
+                'password' => 'required',
+            ]);
+            if($validateUser->fails())
+            {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'error' => $validateUser->errors(),
+                ], 401);
+            }
+            if(!Auth::attempt($request->only(['email', 'password']))){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'username or password does not match!',
+                    'error' => $validateUser->errors(),
+                ], 401);
+            }
+            $user = User::where('email', $request->email)->first();
+            return response()->json([
+                'status' => true,
+                'message' => 'user login successful',
+                'token' => $user->createToken("API TOKEN")->plainTextToken,
+            ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage(),
+            ], 500);
+        }
+    }
+
     public function register(Request $request){
         try {
             $validateUser = Validator::make($request->all(),[
